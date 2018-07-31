60edfe101218edfe9fc9319564e8764b6171a094
Soen, Surya Soenaryo <wei.surya@gmail.com>
2018-07-31 17:54:12 +0700

    First commit

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,117 @@
+#include "kalman_filter.h"
+#include <iostream>
+
+using namespace std;
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+// Please note that the Eigen library does not initialize 
+// VectorXd or MatrixXd objects with zeros upon creation.
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  // New estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+  
+  // KF measurement update step
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
+
+  float rho, phi, rho_dot;
+
+  if(fabs(px) < 0.0001 || fabs(py) < 0.0001) {
+    if(fabs(px) < 0.0001) {
+      cout << "Initialize px is too small. Adjust it into 0.1" << endl;
+      px = 0.0001;
+    }
+
+    if(fabs(py) < 0.0001) {
+      cout << "Initialize py is too small. Adjust it into 0.1" << endl;
+      py = 0.0001;
+    }
+
+    rho = sqrt(px*px + py*py);
+    phi = 0;
+    rho_dot = 0;
+
+  } else {
+    rho = sqrt(px*px + py*py);
+    phi = atan2(py, px);
+    rho_dot = (px*vx + py*vy) / rho;
+  }
+
+  VectorXd z_pred(3);
+  z_pred << rho, phi, rho_dot;
+  VectorXd y = z - z_pred;
+
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  // If phi is not in the range between -phi~phi, adjust it
+  bool phiOK = false;
+  while(phiOK == false) {
+    if(y(1) > 3.14) {
+      y(1) = y(1) - 6.2831;
+    } else if (y(1) < -3.14) {
+      y(1) = y(1) + 6.2831;
+    } else {
+      phiOK = true;
+    }
+  }
+
+  // New estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
